4fb35b14d07082265ce360f50313c2841e42cc71
Almas Akhmetov <awakenedduck@gmail.com>
2023-09-04 17:48:49 +0600

    we

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Hero from "./Components/Hero/Hero"
+
+function App() {
+  return (
+    <div className="text-blue-400 text-4xl font-bold">
+      <Hero />
+    </div>
+  )
+}
+
+export default App
